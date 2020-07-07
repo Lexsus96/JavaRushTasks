29dd3964a20a37b1bb11c5d7b45f40e9d0cd0b27
Алексей Винокуров <avv96@yandex.ru>
2020-07-07 17:29:12 +0300

    [Java Core] 22 done (without project)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,90 @@
+package com.javarush.task.task21.task2109;
+
+import java.util.Objects;
+
+/*
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof A)) return false;
+            A a = (A) o;
+            return i == a.i &&
+                    j == a.j;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
+        }
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof B)) return false;
+            if (!super.equals(o)) return false;
+            B b = (B) o;
+            return Objects.equals(name, b.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), name);
+        }
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class C extends B {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return new C(this.getI(), this.getJ(), this.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
